40eb83179e60bf38e0e568da2717fe9e1efb2820
sazzadur rahman <sazzadurrahman88@gmail.com>
2023-09-09 20:00:10 +0600

    metric calculation done

--- bmi-calculator/js/app.js
+++ bmi-calculator/js/app.js
@@ -1,6 +1,10 @@
 const metricSection = document.getElementById('metric-section')
 const imperialSection = document.getElementById('imperial-section')
+const allRadioButtons = document.querySelectorAll("input[name='bmi_radio']");
 const allInputs = document.querySelectorAll("input[type='text']");
+const metricInputs = document.querySelectorAll("input[class='metric_input']");
+const resultsSection = document.querySelectorAll(".results");
+const welcomeSection = document.querySelectorAll(".welcome");
 
 // in all text input only number is allowed to be inserted
 function validate(event) {
@@ -14,10 +18,9 @@ function validate(event) {
         }
     } else {
         if (!((event.which >= 48 && event.which <= 57)
-            || event.which == 8 || event.which == 46)) {
+            || event.which == 8 || event.which == 46 || event.which == 190)) {
             event.returnValue = false;
         }
-
     }
 }
 
@@ -47,14 +50,66 @@ function unitChangeHandler() {
     }
 }
 
+function validInput(string) {
+    const given = Number(string);
+    return given !== NaN && given > 0;
+}
+
+function showResult(value) {
+    if (value) {
+        Array.prototype.forEach.call(welcomeSection, function (input) {
+            input.style.display = 'none';
+        });
+        Array.prototype.forEach.call(resultsSection, function (input) {
+            input.style.display = 'block';
+        });
+
+        document.getElementById('current_bmi').innerText = value;
+    } else {
+        Array.prototype.forEach.call(resultsSection, function (input) {
+            input.style.display = 'none';
+        });
+        Array.prototype.forEach.call(welcomeSection, function (input) {
+            input.style.display = 'block';
+        });
+    }
+
+}
+
+
+function calculateMetricBMI() {
+    const height = document.getElementById('metric_height').value;
+    const weight = document.getElementById('metric_weight').value;
+
+    if (height && weight) {
+        if (validInput(weight) && validInput(height)) {
+            // (kg / height ^ 2) * 10000
+            const bmi = Number((weight / (height * height)) * 10000).toFixed(1)
+            showResult(bmi)
+        } else {
+            showResult()
+        }
+    } else {
+        showResult()
+    }
+}
+
+function metricUnitChangeHandler(event) {
+    calculateMetricBMI();
+}
+
 
 
 for (let i = 0; i < allInputs.length; i++) {
     allInputs[i].addEventListener('keydown', validate)
 }
 
-const allRadioButtons = document.querySelectorAll("input[name='bmi_radio']");
+
 
 Array.prototype.forEach.call(allRadioButtons, function (radio) {
     radio.addEventListener('change', unitChangeHandler);
+});
+
+Array.prototype.forEach.call(metricInputs, function (input) {
+    input.addEventListener('keyup', metricUnitChangeHandler);
 });
